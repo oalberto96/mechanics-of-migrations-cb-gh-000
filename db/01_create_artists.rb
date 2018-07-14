31b3d59a459b6895b68eccf5d81dabd4eb36d078
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-14 02:11:41 +0000

    automatically backed up by learn

--- db/01_create_artists.rb
+++ db/01_create_artists.rb
@@ -1,6 +1,11 @@
 class CreateArtists < ActiveRecord::Migration
   def up
     create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
   end
 
   def down
